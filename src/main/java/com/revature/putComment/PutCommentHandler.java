9fc170e59f40e06258a25873b4024d0f4bad6fbe
Hiroshi Nobuoka <hiroshi.nobuoka@gmail.com>
2021-09-28 15:09:08 -0400

    changed folder name, sam-template

--- src/main/java/com/revature/putComment/PutCommentHandler.java
+++ src/main/java/com/revature/putComment/PutCommentHandler.java
@@ -1,4 +1,4 @@
-package com.revature.put_comment;
+package com.revature.putComment;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
