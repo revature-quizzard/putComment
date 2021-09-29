f2c1b967ae65b3102e183274344be169c912e183
metteec2 <chmettee@gmail.com>
2021-09-29 11:04:44 -0400

    Added construcotr and update method to CommentsRepo
    Signed-off-by: metteec2 <chmettee@gmail.com>

--- src/main/java/com/revature/put_comment/repos/CommentsRepo.java
+++ src/main/java/com/revature/put_comment/repos/CommentsRepo.java
@@ -1,2 +1,29 @@
-package com.revature.put_comment.repos;public class CommentsRepo {
+package com.revature.put_comment.repos;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.revature.put_comment.models.Comment;
+
+public class CommentsRepo {
+
+    private final DynamoDBMapper dbReader;
+
+    /**
+     * Constructor for initializing the DynamoDBMapper;
+     * @author - Charles Mettee
+     */
+    public CommentsRepo(){
+        dbReader = new DynamoDBMapper(AmazonDynamoDBClientBuilder.defaultClient());
+    }
+
+    /**
+     * @param comment - The Comment object being updated
+     * @author - Charles Mettee
+     */
+    public void updateComment(Comment comment){
+        dbReader.save(comment);
+    }
+
+
+
 }
