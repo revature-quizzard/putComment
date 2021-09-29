b332a53a80aba5502c55cad6b0cc9f309a0c48bd
metteec2 <chmettee@gmail.com>
2021-09-29 11:11:56 -0400

    Added constructor and methods in service class; still need validation logic
    Signed-off-by: metteec2 <chmettee@gmail.com>

--- src/main/java/com/revature/put_comment/services/CommentService.java
+++ src/main/java/com/revature/put_comment/services/CommentService.java
@@ -1,2 +1,36 @@
-package com.revature.put_comment.services;public class CommentService {
+package com.revature.put_comment.services;
+
+import com.revature.put_comment.models.Comment;
+import com.revature.put_comment.repos.CommentsRepo;
+
+public class CommentService {
+
+    private CommentsRepo commentsRepo;
+
+    /**
+     * @param commentsRepo
+     * @author - Charles Mettee
+     */
+    public CommentService(CommentsRepo commentsRepo) {
+        this.commentsRepo = commentsRepo;
+    }
+
+    /**
+     * @param comment - The Comment to be sent to the repo for updating in the database
+     * @throws Exception
+     *
+     * @author - Charles Mettee
+     */
+    public void updateComment(Comment comment) throws Exception{
+        if(!isValid(comment)){
+            throw new Exception("An error occurred");
+        }
+        commentsRepo.updateComment(comment);
+    }
+
+    public boolean isValid(Comment comment){
+        //#TODO add validation logic
+        return true;
+    }
+
 }
