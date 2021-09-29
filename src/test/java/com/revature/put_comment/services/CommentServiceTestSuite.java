c2e2a5b14525bd269a004984e6da93669e725047
metteec2 <chmettee@gmail.com>
2021-09-29 13:06:20 -0400

    Added more unit tests; 100 percent line coverage
    Signed-off-by: metteec2 <chmettee@gmail.com>

--- src/test/java/com/revature/put_comment/services/CommentServiceTestSuite.java
+++ src/test/java/com/revature/put_comment/services/CommentServiceTestSuite.java
@@ -65,7 +65,102 @@ public class CommentServiceTestSuite {
         assertFalse(testResult3);
     }
 
+    /**
+     * author - Charles Mettee
+     */
+    @Test
+    public void isValid_returnsFalse_givenAncestorsNullOrNotSizeTwo(){
+        Comment comment1 = new Comment("commentId", "owner", Arrays.asList("tag1", "tag2"), "2021-09-28T12:05:13.628",
+                null, 0, "description", "threadId", "subject");
+        Comment comment2 = new Comment("commentId", "owner", Arrays.asList("tag1", "tag2"), "2021-09-28T12:05:13.628",
+                Arrays.asList("threadId"), 0, "description", "threadId", "subject");
 
+        boolean testResult1 = sut.isValid(comment1);
+        boolean testResult2 = sut.isValid(comment2);
+
+        assertFalse(testResult1);
+        assertFalse(testResult2);
+    }
+
+    /**
+     * author - Charles Mettee
+     */
+    @Test
+    public void isValid_returnsFalse_givenNullOrEmptyParent(){
+        Comment comment1 = new Comment("commentId", "owner", Arrays.asList("tag1", "tag2"), "2021-09-28T12:05:13.628",
+                Arrays.asList("subforumId", "threadId"), 0, "description", null, "subject");
+        Comment comment2 = new Comment("commentId", "owner", Arrays.asList("tag1", "tag2"), "2021-09-28T12:05:13.628",
+                Arrays.asList("subforumId", "threadId"), 0, "description", "", "subject");
+        Comment comment3 = new Comment("commentId", "owner", Arrays.asList("tag1", "tag2"), "2021-09-28T12:05:13.628",
+                Arrays.asList("subforumId", "threadId"), 0, "description", "     ", "subject");
+
+        boolean testResult1 = sut.isValid(comment1);
+        boolean testResult2 = sut.isValid(comment2);
+        boolean testResult3 = sut.isValid(comment3);
+
+        assertFalse(testResult1);
+        assertFalse(testResult2);
+        assertFalse(testResult3);
+    }
+
+    /**
+     * author - Charles Mettee
+     */
+    @Test
+    public void isValid_returnsFalse_givenNullOrEmptyDescription(){
+        Comment comment1 = new Comment("commentId", "owner", Arrays.asList("tag1", "tag2"), "2021-09-28T12:05:13.628",
+                Arrays.asList("subforumId", "threadId"), 0, null, "threadId", "subject");
+        Comment comment2 = new Comment("commentId", "owner", Arrays.asList("tag1", "tag2"), "2021-09-28T12:05:13.628",
+                Arrays.asList("subforumId", "threadId"), 0, "", "threadId", "subject");
+        Comment comment3 = new Comment("commentId", "owner", Arrays.asList("tag1", "tag2"), "2021-09-28T12:05:13.628",
+                Arrays.asList("subforumId", "threadId"), 0, "     ", "threadId", "subject");
+
+        boolean testResult1 = sut.isValid(comment1);
+        boolean testResult2 = sut.isValid(comment2);
+        boolean testResult3 = sut.isValid(comment3);
+
+        assertFalse(testResult1);
+        assertFalse(testResult2);
+        assertFalse(testResult3);
+    }
+
+    /**
+     * author - Charles Mettee
+     */
+    @Test
+    public void isValid_returnsFalse_givenNullOrInvalidDate(){
+        Comment comment1 = new Comment("commentId", "owner", Arrays.asList("tag1", "tag2"), null,
+                Arrays.asList("subforumId", "threadId"), 0, "description", "threadId", "subject");
+        Comment comment2 = new Comment("commentId", "owner", Arrays.asList("tag1", "tag2"), "date",
+                Arrays.asList("subforumId", "threadId"), 0, "description", "threadId", "subject");
+
+        boolean testResult1 = sut.isValid(comment1);
+        boolean testResult2 = sut.isValid(comment2);
+
+        assertFalse(testResult1);
+        assertFalse(testResult2);
+    }
+
+    /**
+     * author - Charles Mettee
+     */
+    @Test
+    public void isValid_returnsFalse_givenNullOrEmptyOwner(){
+        Comment comment1 = new Comment("commentId", null, Arrays.asList("tag1", "tag2"), "2021-09-28T12:05:13.628",
+                Arrays.asList("subforumId", "threadId"), 0, "description", "threadId", "subject");
+        Comment comment2 = new Comment("commentId", "", Arrays.asList("tag1", "tag2"), "2021-09-28T12:05:13.628",
+                Arrays.asList("subforumId", "threadId"), 0, "description", "threadId", "subject");
+        Comment comment3 = new Comment("commentId", "     ", Arrays.asList("tag1", "tag2"), "2021-09-28T12:05:13.628",
+                Arrays.asList("subforumId", "threadId"), 0, "description", "threadId", "subject");
+
+        boolean testResult1 = sut.isValid(comment1);
+        boolean testResult2 = sut.isValid(comment2);
+        boolean testResult3 = sut.isValid(comment3);
+
+        assertFalse(testResult1);
+        assertFalse(testResult2);
+        assertFalse(testResult3);
+    }
 
     /**
      * author - Charles Mettee
@@ -78,6 +173,41 @@ public class CommentServiceTestSuite {
         boolean testResult = sut.isValid(comment1);
 
         assertTrue(testResult);
+    }
+
+    /**
+     * author - Charles Mettee
+     */
+    @Test
+    public void updateThreads_throwsException_givenInvalidThread(){
+        Comment comment = null;
+        boolean testResult = false;
+
+        try{
+            sut.updateComment(comment);
+        } catch (Exception e){
+            testResult = true;
+        }
+
+        assertTrue(testResult);
+    }
+
+    /**
+     * author - Charles Mettee
+     */
+    @Test
+    public void updateThreads_isSuccessful_givenValidThread(){
+        Comment comment = new Comment("commentId", "owner", Arrays.asList("tag1", "tag2"), "2021-09-28T12:05:13.628",
+                Arrays.asList("subforumId", "threadId"), 0, "description", "threadId", "subject");
+
+        try{
+            sut.updateComment(comment);
+        } catch (Exception e){
+
+        }
+
+        verify(mockCommentsRepo, times(1)).updateComment(comment);
+
     }
 
 
